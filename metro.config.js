1555b7c6ea21041bc322e18ba989436b8879905b
Sarvesh M <iglost999@gmail.com>
2024-06-05 15:05:00 +0530

    removed unwanted code in app.tsx

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,4 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
 
 /**
  * Metro configuration
@@ -7,5 +7,4 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  * @type {import('metro-config').MetroConfig}
  */
 const config = {};
-
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
