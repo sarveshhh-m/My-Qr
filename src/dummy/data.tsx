82fbd6a7ec05008acbfdff4049dcfb71131b0a86
Sarvesh M <iglost999@gmail.com>
2024-06-05 18:25:47 +0530

    crafted HomeScreen

---
+++ src/dummy/data.tsx
@@ -0,0 +1,24 @@
+export default {
+    homeScreenData: [
+        {
+            id: 1,
+            name: "clipboard",
+            color: "#ffd991"
+        },
+        {
+            id: 2,
+            name: "URL",
+            color: "#6bb8ff"
+        },
+        {
+            id: 3,
+            name: "Wi-fi",
+            color: "#fe266e"
+        },
+        {
+            id: 4,
+            name: "Phone",
+            color: "#ddffdd"
+        }
+    ]
+}
--- src/screens/app/Home.tsx
+++ src/screens/app/Home.tsx
@@ -1,10 +1,26 @@
 import { Text, View } from '@gluestack-ui/themed'
 import React from 'react'
+import data from '../../dummy/data'
+import { Dimensions, FlatList } from 'react-native'
+
 
 const Home = () => {
+    const { width } = Dimensions.get('window')
     return (
-        <View>
-            <Text>Home</Text>
+        <View backgroundColor='#121624' flex={1}>
+            <View flex={1} backgroundColor='#f2f2f2' borderTopLeftRadius={40} borderTopRightRadius={40}>
+                <FlatList
+                    style={{ padding: 16 }}
+                    contentContainerStyle={{ alignItems: 'center' }}
+                    numColumns={2}
+
+                    data={data.homeScreenData}
+                    renderItem={({ item }) => (<View borderRadius={"$3xl"} p={16} rowGap={"$4"} bg='#ffffff' width={width / 2 - 50} alignItems='center' m={"$2"} >
+                        <View height={72} width={72} bg={item.color} rounded={"$full"} />
+                        <Text>{item.name}</Text>
+                    </View>)}
+                />
+            </View>
         </View>
     )
 }
